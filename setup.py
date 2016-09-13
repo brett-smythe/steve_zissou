d75951f40d12e636dea4dccf4baa689fd43c6c24
brett <brett@debian-laptop>
2016-09-13 12:45:39 -0700

    Updated for client lib static IPs

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as inf:
 
 setup(
     name='steve-zissou',
-    version='0.0.0.1',
+    version='0.0.0.2',
     description='Web app for displaying data collected from various sources',
     author='Brett Smythe',
     author_email='smythebrett@gmail.com',
