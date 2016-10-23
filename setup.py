3b90de3a270f73708a0447bebf366e0dce8f0c4f
brett <brett@debian-laptop>
2016-10-22 22:30:06 -0700

    Moved static files to nginx after figuring out the dumb thing I was doing

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as inf:
 
 setup(
     name='steve-zissou',
-    version='0.0.3',
+    version='0.0.4',
     description='Web app for displaying data collected from various sources',
     author='Brett Smythe',
     author_email='smythebrett@gmail.com',
